c258985739938a1cbc6691afda5130226f16392b
Agnieszka Nowakowska <agnieszkanowakowska@iMac-Agnieszka.local>
2020-10-27 23:05:31 +0100

    Add checkbox function, work in progress

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 
-let myList = document.getElementsByTagName("li");
+let myList = document.getElementsByTagName('li');
 let i;
 
-document.getElementById("add-task").addEventListener('click', newListElement);
-document.getElementById("add-task").addEventListener('click', clearInput);
+document.getElementById('add-task').addEventListener('click', newListElement);
+document.getElementById('add-task').addEventListener('click', clearInput);
 
 function newListElement() {
-    let li = document.createElement("li");
-    let inputValue = document.getElementById("input-task").value;
+    let li = document.createElement('li');
+    let inputValue = document.getElementById('input-task').value;
     let inputText = document.createTextNode(inputValue);
 
     li.appendChild(inputText);
 
     if (inputValue === '') {
-        alert("You must write the task!");
+        alert('You must write the task!');
      } else {
-        document.getElementById("list").appendChild(li);
+        document.getElementById('list').appendChild(li);
      }
 }
 
 function clearInput() {
-    document.getElementById("input-task").value='';
+    document.getElementById('input-task').value='';
 }
 
+function checkLineThrough() {
+    if (checkbox.checked == true) {
+        task.style.textDecoration = 'line-through';
+    } else {
+        task.style.textDecoration = 'none';
+
+    }
+}
+
+let checkbox = document.getElementsByClassName('box');
+let task = document.getElementsByClassName('list-task');
+
+let taskList = document.querySelectorAll('.list-task');
+for (let i = 0; i < taskList.length; i++) {
+    taskList[i].addEventListener('click', checkLineThrough);
+} 
